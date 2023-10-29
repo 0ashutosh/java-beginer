b977286690e3fd0e5fb45a5d58f5533a09d6edad
perpendicular01 <53850029+0ashutosh@users.noreply.github.com>
2023-10-28 23:37:51 -0700

    Add files via upload

---
+++ JavaBeginner/Planet.java
@@ -0,0 +1,11 @@
+ class Planet{
+         
+           void revolve(){//method 
+           System.out.println("Revolve");
+           }
+        public static void main(String[] args){
+		Planet earth = new Planet();//object of class 
+		earth.revolve();
+		}
+    
+       }
